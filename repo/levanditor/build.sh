facbcc1421701f91ab0b84272a62a8cc5846cd5d
Dan <97045983+DevDan0@users.noreply.github.com>
2022-01-04 16:52:40 +0530

    Add files via upload

--- repo/levanditor/build.sh
+++ repo/levanditor/build.sh
@@ -1,1 +1,31 @@
+pkgname=levditor
+pkgver=master
+LICENSE=LICENSE
+
+fetch() {
+	curl -L https://github.com/DevDan0/Levanditor/archive/refs/tags/Release.tar.gz -o $pkgname-$pkgver.tar.gz
+	tar -xvf $pkgname-$pkgver.tar.gz
+}
+
+build() {
+	cd $pkgname-$pkgver
+	echo "Nothing to do"
+}
+
+package() {
+	cd $pkgname-$pkgver/Editor
+	sh ./installer.sh
+}
+
+uninstall() {
+  cd $pkgname-$pkgver/Editor
+  sh ./uninstall.sh
+}
+
+LICENSE() {
+  echo "LICENSE: N/A"
+}
+
+
+
 
