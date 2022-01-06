14dbfb4e44234f0365fe2b9580f9271bf8e46a2b
MrVeink <59528135+MrVeink@users.noreply.github.com>
2022-01-06 23:18:57 +0200

    Changed source

--- repo/byacc/build.sh
+++ repo/byacc/build.sh
@@ -1,25 +1,25 @@
 pkgname=byacc
-pkgver=20210808
+pkgver=24_12_21
 LICENSE=LICENSE
 
 fetch() {
-        curl -L "https://invisible-island.net/datafiles/release/byacc.tar.gz" -o $pkgname-$pkgver.tar.gz
-	tar -xf $pkgname-$pkgver.tar.gz
+        curl -L "http://nic.orangeos.xyz:8906/devel/byacc/$pkgname-$pkgver.tar.xz" -o $pkgname-$pkgver.tar.xz
+	tar -xf $pkgname-$pkgver.tar.xz
 }
 
 build() {
-        cd $pkgname-$pkgver
+        cd $pkgname
         ./configure --prefix=/usr
         make
 }
 
 package() {
-        cd $pkgname-$pkgver
+        cd $pkgname
         make install
 }
 
 license() {
-        cd $pkgname-$pkgver
+        cd $pkgname
         cat $LICENSE
 }
 
